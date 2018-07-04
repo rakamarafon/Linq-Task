f45a357076553dfd2783fdfac3536bd1a855320a
Maksym Akulov <maksakulov@hotmail.com>
2018-07-04 11:46:16 +0300

    Added interface for DAL(fixes #4)

---
+++ Linq-Task/DAL/IHttpDataSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq_Task.DAL
+{
+    interface IHttpDataSource
+    {
+        List<Users> GetUsersList();
+        List<Posts> GetPostsList();
+        List<Comments> GetCommentsList();
+        List<Todos> GetTodosList();
+        List<Address> GetAddressesList();
+    }
+}
