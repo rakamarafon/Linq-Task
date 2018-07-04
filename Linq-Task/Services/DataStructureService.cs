4426101ee41de377b7e4369d00c8b6538741f0c1
Maksym Akulov <maksakulov@hotmail.com>
2018-07-04 22:59:16 +0300

    created structure from api's(fixes #8)

--- Linq-Task/Services/DataStructureService.cs
+++ Linq-Task/Services/DataStructureService.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Linq_Task.Services
 {
     public class DataStructureService : IDataStructureService
-    {        
+    {
+        private class MainCollection
+        {
+            public int Uid { get; set; }
+            public DateTime CreatedAt { get; set; }
+            public string Name { get; set; }
+            public string Avatar { get; set; }
+            public string Email { get; set; }
+            public IEnumerable<Posts> UPost { get; set; }
+            public IEnumerable<Comments> PComments { get; set; }
+        }
+
+        private List<MainCollection> collection = new List<MainCollection>();
+
         public DataStructureService(List<Address> addresses, List<Comments> comments, List<Posts> posts, List<Todos> todos, List< Users> users)
         {
             var commentsToPosts = posts.GroupJoin(
@@ -34,7 +48,6 @@ namespace Linq_Task.Services
 
             var postToUsers = users.GroupJoin(
                                         commentsToPosts,
-                                        //posts,
                                         u => u.id,
                                         p => p.UserId,
                                         (user, pst) => new
@@ -44,27 +57,47 @@ namespace Linq_Task.Services
                                             Name = user.name,
                                             Avatar = user.avatar,
                                             Email = user.email,
-                                            UPosts = pst.Select(p => new
+                                            UPosts = pst.Select(p => new Posts()
                                             {
                                                 id = p.Id,
-                                                CreatedAt = p.CreadetAt,
+                                                createdAt = p.CreadetAt,
                                                 title = p.Title,
                                                 body = p.Body,
                                                 userId = p.UserId,
-                                                likes = p.Likes,                                                
-                                                PComments = com.Select(c => new
-                                                 {
-                                                     Id = c.id,
-                                                     CreadetAt = c.createdAt,
-                                                     Body = c.body,
-                                                     UserId = c.userId,
-                                                     PostId = c.postId,
-                                                     Likes = c.likes
-                                                 })
+                                                likes = p.Likes
                                             })
                                         });
-           
-            var todosToUsers = postToUsers.
+
+            var UsersWithPostsCommentsTodos = postToUsers.GroupJoin(
+                                                comments,
+                                                p => p.Id,
+                                                c => c.userId,
+                                                (pst, com) => new
+                                                {
+                                                    Uid = pst.Id,
+                                                    pst.CreatedAt,
+                                                    pst.Name,
+                                                    pst.Avatar,
+                                                    pst.Email,
+                                                    pst.UPosts,
+                                                    PComments = com.Select(c => new Comments()
+                                                    {
+                                                        id = c.id,
+                                                        createdAt = c.createdAt,
+                                                        body = c.body,
+                                                        userId = c.userId,
+                                                        postId = c.postId,
+                                                        likes = c.likes
+                                                    })
+                                                });
+
+            foreach (var item in UsersWithPostsCommentsTodos)
+            {
+                MainCollection temp = new MainCollection() {Uid = item.Uid, CreatedAt = item.CreatedAt, Name = item.Name, Avatar = item.Avatar, Email = item.Email, PComments = item.PComments, UPost = item.UPosts };
+                collection.Add(temp);
+
+            }
+            
         }
         public IEnumerable<(Posts, int)> GetCommentsCountByUserPosts(int user_id, List<Posts> posts, List<Comments> comments)
         {
